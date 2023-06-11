04c8c3f42d432ff2c19e84be2783718579ee539a
zahrasalsabila12 <91620952+zahrasalsabila12@users.noreply.github.com>
2023-06-11 23:59:36 +0800

    Add files via upload

---
+++ UAS/src/uas/main.java
@@ -0,0 +1,12 @@
+package uas;
+
+public class main {
+
+    public static void main(String[] args) {
+        Login LoginFrame = new Login();
+        LoginFrame.setVisible(true);
+        LoginFrame.pack();
+        LoginFrame.setLocationRelativeTo(null); 
+    }
+    
+}
